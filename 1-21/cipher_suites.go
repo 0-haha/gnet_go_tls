a5ca40dfc279f255631a25a8d27ef5bfe2ec93af
0-haha <65914167+0-haha@users.noreply.github.com>
2023-04-05 06:56:52 +0000

    prepare for go 1.21

--- 1-21/cipher_suites.go
+++ 1-21/cipher_suites.go
@@ -17,7 +17,7 @@ import (
 	"hash"
 	"runtime"
 
-	"github.com/0-haha/gnet_go_tls/1-20/internal/boring"
+	"github.com/0-haha/gnet_go_tls/1-21/internal/boring"
 	"golang.org/x/crypto/chacha20poly1305"
 	"golang.org/x/sys/cpu"
 )
